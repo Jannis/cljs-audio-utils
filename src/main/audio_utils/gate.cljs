a00a49a54cdd67a7ec48a62c0505745ddb82d2ee
Jannis Pohlmann <contact@jannispohlmann.de>
2016-03-01 18:33:50 +0100

    Prepare gate for configuration

--- src/main/audio_utils/gate.cljs
+++ src/main/audio_utils/gate.cljs
@@ -1,11 +1,28 @@
 (ns audio-utils.gate
-  (:require [audio-utils.util :refer [aatom aderef aswap!]]))
+  (:require [audio-utils.ring-buffer :as rb]
+            [audio-utils.rms-buffer :as rms]
+            [audio-utils.util :refer [db->amplitude time->samples]]))
 
 (defprotocol IGate)
 
-(deftype Gate []
-  IGate)
+(defn default-trigger
+  [gate state])
 
 (defn gate
-  []
-  (Gate.))
+  [{:keys [audio-ctx size threshold look-ahead hold rms-window trigger]
+    :or   {size       8192
+           threshold  -32.0
+           look-ahead 0.5
+           hold       0.1
+           rms-window 0.1
+           trigger    default-trigger}}]
+  (let [processor          (.createScriptProcessor audio-ctx size 1 1)
+        sample-rate        (.-sampleRate audio-ctx)
+        threshold'         (db->amplitude threshold)
+        rms-samples        (time->samples rms-window sample-rate)
+        hold-samples       (time->samples hold sample-rate)
+        look-ahead-samples (time->samples look-ahead sample-rate)
+        rms-buffer         (rms/rms-buffer rms-samples)
+        look-ahead-buffer  (rb/ring-buffer look-ahead-samples)]
+    (specify! processor
+      IGate)))
--- src/main/audio_utils/util.cljs
+++ src/main/audio_utils/util.cljs
@@ -1,5 +1,7 @@
 (ns audio-utils.util)
 
+;;;; Pseudo atom based on a JS array
+
 (defn aatom
   [x]
   (to-array [x]))
@@ -11,3 +13,38 @@
 (defn aderef
   [x]
   (aget x 0))
+
+;;;; Web audio polyfills
+
+(defn audio-context []
+  (let [constructor (or js/window.AudioContext
+                        js/window.webkitAudioContext)]
+    (constructor.)))
+
+(defn get-user-media
+  [constraints cb log]
+  (let [media-fn (or js/navigator.getUserMedia
+                     js/navigator.webkitGetUserMedia
+                     js/navigator.mozGetUserMedia
+                     js/navigator.msGetUserMedia)]
+    ((.bind media-fn js/navigator) (clj->js constraints) cb log)))
+
+;;;; Time/samples and dB/amplitude conversion
+
+(defn db->amplitude
+  "Convert a dB value such as -6.0 to an amplitude value between 0.0
+   and 1.0, such as 0.5011872336272722."
+  [x]
+  (Math/pow 10 (/ x 20)))
+
+(defn amplitude->db
+  "Convert an amplitude value between -1.0 and 1.0, such as
+   0.5011872336272722, to a dB value like -6.0."
+  [x]
+  (* 20 (Math/log10 x)))
+
+(defn time->samples
+  "Converts a time in milliseconds to the corresponding number of
+   samples given a sample rate."
+  [x sample-rate]
+  (long (* (/ x 1000) sample-rate)))
