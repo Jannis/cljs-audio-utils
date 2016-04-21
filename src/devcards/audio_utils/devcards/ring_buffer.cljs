5b23e45722317f90896b775ece945e78edfc6272
Jannis Pohlmann <contact@jannispohlmann.de>
2016-04-21 21:47:03 +0200

    Improve RingBuffer visualization descriptions and add another test

--- src/devcards/audio_utils/devcards/ring_buffer.cljs
+++ src/devcards/audio_utils/devcards/ring_buffer.cljs
@@ -51,20 +51,21 @@
          [:div {:style {:text-align "center"}} "Actual"]]]]])))
 
 (defcard
-  "100 zeroes in a ring buffer of size 100.
-   100 values taken out of it."
+  "100 zeroes in a ring buffer of size 100. Taking 100 values
+   out of it results in 100 values of 0.0."
   (let [data (repeat 100 0.0)]
     (plot-buffer 100 data (into (ring-buffer 100) data))))
 
 (defcard
-  "100 zeroes in a ring buffer of size 100.
-   200 values taken out of it."
+  "100 zeroes in a ring buffer of size 100. Taking 200 values
+   out of it results in 100 values of 0.0, followed by 100
+   nils."
   (let [data (repeat 100 0.0)]
     (plot-buffer 200 data (into (ring-buffer 100) data))))
 
 (defcard
   "100 zeroes and 100 ones in a ring buffer of size 100.
-   100 values taken out of it."
+   Taking 100 values out of results in 100 values of 1.0."
   (let [data (concat (repeat 100 0.0) (repeat 100 1.0))]
     (plot-buffer 100
                  (drop 100 data)
@@ -72,7 +73,8 @@
 
 (defcard
   "100 values linearly distributed between -1.0 and 1.0
-   in ring buffer of size 100. 10 values taken out of it."
+   in ring buffer of size 100. Taking 10 values out of
+   it results in 10 values starting at -1.0."
   (let [data (linear-distribution [-1.0 1.0] 100)]
     (plot-buffer 10
                  (take 10 data)
@@ -80,15 +82,8 @@
 
 (defcard
   "100 values linearly distributed between -1.0 and 1.0
-   in ring buffer of size 100. 20 values taken out of it."
-  (let [data (linear-distribution [-1.0 1.0] 100)]
-    (plot-buffer 20
-                 (take 20 data)
-                 (into (ring-buffer 100) data))))
-
-(defcard
-  "100 values linearly distributed between -1.0 and 1.0
-   in ring buffer of size 100. 100 values taken out of it."
+   in ring buffer of size 100. Taking 100 values out of
+   it results in 100 values from -1.0 to 1.0."
   (let [data (linear-distribution [-1.0 1.0] 100)]
     (plot-buffer 100
                  (take 100 data)
@@ -96,7 +91,9 @@
 
 (defcard
   "100 values linearly distributed between -1.0 and 1.0
-   in ring buffer of size 100. 200 values taken out of it."
+   in ring buffer of size 100. Taking 200 values out of
+   it results in 100 values from -1.0 and 1.0, followed
+   by 100 nils."
   (let [data (linear-distribution [-1.0 1.0] 100)]
     (plot-buffer 200
                  (take 100 data)
@@ -104,8 +101,25 @@
 
 (defcard
   "200 values linearly distributed between -1.0 and 1.0
-   in ring buffer of size 100. 200 values taken out of it."
+   in ring buffer of size 100. Adding the 200 values into
+   the smaller buffer means that the first 100 values from
+   -1.0 to 0.0 are replaced with the second 100 values from
+   0.0 to 1.0. Taking 200 values out results in 100 values
+   from 0.0 to 1.0, followed by 100 nils."
   (let [data (linear-distribution [-1.0 1.0] 200)]
     (plot-buffer 200
                  (drop 100 data)
+                 (into (ring-buffer 100) data))))
+
+(defcard
+  "300 values linearly distributed between -1.0 and 1.0
+   in ring buffer of size 100. Adding the 300 values into
+   the smaller buffer means that the first 100 values from
+   -1.0 to -0.33 are replaced with the second 100 values from
+   -0.33 to 0.33 and then replaced with the third 100 values
+   from 0.33 to 1.0. Taking 200 values out results in 100
+   values from -0.33 to 1.0, followed by 100 nils."
+  (let [data (linear-distribution [-1.0 1.0] 300)]
+    (plot-buffer 200
+                 (drop 200 data)
                  (into (ring-buffer 100) data))))
