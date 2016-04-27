95240e7d73a4dc74766ac37357f4c09b6aebdee2
Jannis Pohlmann <contact@jannispohlmann.de>
2016-04-27 10:45:17 +0200

    Add create-buffer and create-buffer-source to audio-utils.web-audio

--- src/main/audio_utils/web_audio.cljs
+++ src/main/audio_utils/web_audio.cljs
@@ -3,3 +3,27 @@
 (defn audio-context []
   (or (new js/window.AudioContext)
       (new js/window.webkitAudioContext)))
+
+(defn create-buffer
+  [ctx n-channels size sample-rate]
+  (.createBuffer ctx n-channels size sample-rate))
+
+(defn create-buffer-source
+  ([ctx]
+   (.createBufferSource ctx))
+  ([ctx n-channels sample-rate data]
+   (let [buf (create-buffer ctx n-channels
+                            (count (cond-> data
+                                     (> n-channels 1)
+                                     first))
+                            sample-rate)
+         src (create-buffer-source ctx)]
+     (doseq [channel (range 0 n-channels)
+             :let [channel-data (.getChannelData buf channel)
+                   input-data   (cond-> data
+                                  (> n-channels 1)
+                                  (get channel))]]
+       (doseq [[i x] (map-indexed vector input-data)]
+         (aset channel-data i x)))
+     (set! (.-buffer src) buf)
+     src)))
