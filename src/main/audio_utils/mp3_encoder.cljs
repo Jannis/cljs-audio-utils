58a4a893b93a84ed43e2832d90428de6b62888c8
Jannis Pohlmann <contact@jannispohlmann.de>
2016-05-09 14:33:13 +0200

    Add MP3 encoder implementation for worker audio pipelines
    An important part of this is to translate float sample vectors to
    integer arrays for encoding since that's what lamejs expects as its
    input.

---
+++ src/main/audio_utils/mp3_encoder.cljs
@@ -0,0 +1,31 @@
+(ns audio-utils.mp3-encoder
+  (:require [cljsjs.lamejs]
+            [audio-utils.worker :as w]))
+
+(defrecord MP3Encoder [bit-rate sample-rate next]
+  w/IWorkerAudioNode
+  (connect [this destination]
+    (reset! next destination))
+
+  (disconnect [this]
+    (reset! next nil))
+
+  (process-audio [this data]
+    (let [int-data (mapv (fn [samples]
+                           (into-array (map #(* 32768 %) samples)))
+                         data)
+          encoder  (js/lamejs.Mp3Encoder. 2 sample-rate bit-rate)
+          encoded  #js []]
+      (doto encoded
+        (.push (apply (aget encoder "encodeBuffer") int-data))
+        (.push (.flush encoder)))
+      (some-> @next (w/process-audio encoded)))))
+
+(defn mp3-encoder
+  [{:keys [bit-rate
+           sample-rate]
+    :or   {bit-rate    128
+           sample-rate 44100}}]
+  (map->MP3Encoder {:bit-rate    bit-rate
+                    :sample-rate sample-rate
+                    :next        (atom nil)}))
