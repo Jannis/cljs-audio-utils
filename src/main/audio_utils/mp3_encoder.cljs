fb28c62fe33a76dab5d6dad0db09b675079c98c4
Jannis Pohlmann <contact@jannispohlmann.de>
2016-05-16 11:18:26 +0200

    WIP: Work on getting the MP3 encoder to work correctly

--- src/main/audio_utils/mp3_encoder.cljs
+++ src/main/audio_utils/mp3_encoder.cljs
@@ -11,11 +11,22 @@
     (reset! next nil))
 
   (process-audio [this data]
-    (let [encoder (js/lamejs.Mp3Encoder. (count data) sample-rate
-                                         bit-rate)
-          samples (into-array (map into-array data))
-          encoded (.encodeBuffer encoder samples)]
-      (some-> @next (w/process-audio (js->clj encoded))))))
+    (js/console.log "MP3 ENCODER PROCESS AUDIO"
+                    data
+                    (type data)
+                    (count data)
+                    (type (data 0))
+                    (count (data 0)))
+    (let [encoder (js/lamejs.Mp3Encoder. 1 sample-rate bit-rate)
+          samples (mapv (fn [channel-data]
+                          (js/Float32Array. (into-array channel-data)))
+                        data)
+          encoded #js []]
+      (js/console.log "SAMPLES" samples (samples 0))
+      (doto encoded
+        (.push (.encodeBuffer encoder (samples 0)))
+        (.push (.flush encoder)))
+      (some-> @next (w/process-audio encoded)))))
 
 (defn mp3-encoder
   [{:keys [bit-rate
