e9db29b4e52da7404995d3f8820cf2926f925c2f
Jannis Pohlmann <contact@jannispohlmann.de>
2016-05-30 23:52:40 +0200

    Add function to create an AudioBuffer from CLJS data

--- src/main/audio_utils/web_audio.cljs
+++ src/main/audio_utils/web_audio.cljs
@@ -6,25 +6,30 @@
     (constructor.)))
 
 (defn create-buffer
-  [ctx n-channels size sample-rate]
-  (.createBuffer ctx n-channels size sample-rate))
+  ([ctx n-channels size sample-rate]
+   (.createBuffer ctx n-channels size sample-rate)))
+
+(defn create-buffer-from-data
+  [ctx n-channels sample-rate data]
+  (let [buf (create-buffer ctx n-channels
+                           (count (cond-> data
+                                    (> n-channels 1)
+                                    first))
+                           sample-rate)]
+    (doseq [channel (range 0 n-channels)
+            :let [channel-data (.getChannelData buf channel)
+                  input-data   (cond-> data
+                                 (> n-channels 1)
+                                 (get channel))]]
+      (doseq [[i x] (map-indexed vector input-data)]
+        (aset channel-data i x)))
+    buf))
 
 (defn create-buffer-source
   ([ctx]
    (.createBufferSource ctx))
   ([ctx n-channels sample-rate data]
-   (let [buf (create-buffer ctx n-channels
-                            (count (cond-> data
-                                     (> n-channels 1)
-                                     first))
-                            sample-rate)
-         src (create-buffer-source ctx)]
-     (doseq [channel (range 0 n-channels)
-             :let [channel-data (.getChannelData buf channel)
-                   input-data   (cond-> data
-                                  (> n-channels 1)
-                                  (get channel))]]
-       (doseq [[i x] (map-indexed vector input-data)]
-         (aset channel-data i x)))
-     (set! (.-buffer src) buf)
-     src)))
+   (doto (create-buffer-source ctx)
+     (aset "buffer"
+           (create-buffer-from-data ctx n-channels
+                                    sample-rate data)))))
